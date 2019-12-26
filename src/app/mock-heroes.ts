cf0467c8377c508471d9409be466cb073f54aaa3
Taildh <lehuudinhtai@gmail.com>
2019-12-26 11:05:05 +0700

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+	{id: 1, name: 'Dr Strange'},
+	{id: 2, name: 'Iron Man'},
+	{id: 3, name: 'Captain America'},
+	{id: 4, name: 'Ant Man'},
+	{id: 5, name: 'Spider Man'},
+	{id: 6, name: 'Hulk'},
+	{id: 7, name: 'Thor'},
+	{id: 8, name: 'Black Widow'},
+	{id: 9, name: 'Thanos'},
+]
